a679c0f13ceb35157267329451bee9bb792f4db6
anasmebarki1996 <anasmebarki1996@outlook.fr>
2020-03-26 21:07:14 +0100

    some changes

--- src/views/Dashboards/Intervention.vue
+++ src/views/Dashboards/Intervention.vue
@@ -1,28 +1,30 @@
 <template>
   <div>
-    <page-title
-      :heading="heading"
-      :subheading="subheading"
-      :icon="icon"
-    ></page-title>
+    <page-title :heading="heading" :subheading="subheading" :icon="icon"></page-title>
 
     <b-table :items="items" :fields="fields" striped responsive="sm">
       <template v-slot:cell(show_details)="row">
-        <b-button size="sm" @click="row.toggleDetails" class="mr-2">
-          {{ row.detailsShowing ? "Hide" : "Show" }} Details
-        </b-button>
+        <b-button
+          size="sm"
+          @click="row.toggleDetails"
+          class="mr-2"
+        >{{ row.detailsShowing ? "Hide" : "Show" }} Details</b-button>
       </template>
 
       <template v-slot:row-details="row">
         <b-card>
           <b-row class="mb-2">
-            <b-col sm="3" class="text-sm-right"><b>Age:</b></b-col>
-            <b-col>{{ row.item.age }}</b-col>
+            <b-col sm="3" class="text-sm-right">
+              <b>numTel:</b>
+            </b-col>
+            <b-col>{{ row.item.numTel }}</b-col>
           </b-row>
 
           <b-row class="mb-2">
-            <b-col sm="3" class="text-sm-right"><b>Is Active:</b></b-col>
-            <b-col>{{ row.item.isActive }}</b-col>
+            <b-col sm="3" class="text-sm-right">
+              <b>gps_coordonnee:</b>
+            </b-col>
+            <b-col>{{ row.item.gps_coordonnee }}</b-col>
           </b-row>
         </b-card>
       </template>
@@ -32,9 +34,15 @@
         <li class="page-item disabled">
           <a class="page-link" href="#" tabindex="-1">Previous</a>
         </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
+        <li class="page-item">
+          <a class="page-link" href="#">1</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">2</a>
+        </li>
+        <li class="page-item">
+          <a class="page-link" href="#">3</a>
+        </li>
         <li class="page-item">
           <a class="page-link" href="#">Next</a>
         </li>
@@ -45,6 +53,7 @@
 
 <script>
 import PageTitle from "../../Layout/Components/PageTitle";
+const axios = require("axios");
 
 export default {
   components: {
@@ -52,29 +61,29 @@ export default {
   },
   data() {
     return {
-      heading: "Analytics Dashboard",
+      heading: "Les intervention",
       subheading:
         "This is an example dashboard created using build-in elements and components.",
       icon: "pe-7s-plane icon-gradient bg-tempting-azure",
-      fields: ["first_name", "last_name", "show_details"],
-      items: [
-        {
-          isActive: true,
-          age: 40,
-          first_name: "Dickerson",
-          last_name: "Macdonald"
-        },
-        { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
-        {
-          isActive: false,
-          age: 89,
-          first_name: "Geneva",
-          last_name: "Wilson",
-          _showDetails: true
-        },
-        { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" }
-      ]
+      fields: ["dateTimeAppel", "gps_coordonnee", "show_details"],
+      items: []
     };
+  },
+  methods: {
+    getAllIntervention() {
+      axios
+        .post("http://localhost:8000/API/getAllIntervention", {})
+        .then(res => {
+          console.log(res.data.data.interventions);
+          this.items = res.data.data.interventions;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  },
+  created() {
+    this.getAllIntervention();
   }
 };
 </script>
